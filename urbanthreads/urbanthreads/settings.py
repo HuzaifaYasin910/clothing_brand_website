e5f2bd2d57b421b4945dfae42f8b55f64b5cd710
HuzaifaYasin910 <huzaifayasin.910@gmail.com>
2023-11-16 22:13:44 +0500

    update allowed host

--- urbanthreads/urbanthreads/settings.py
+++ urbanthreads/urbanthreads/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9ez5f7gk!a=l&w#v7@+$tzu9p+gsj^xdkqf*5(jaejoh&=!ex(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

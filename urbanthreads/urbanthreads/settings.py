d2df3b5ff836ed5c2be9b2bd31a23ab83f06069a
HuzaifaYasin910 <huzaifayasin.910@gmail.com>
2023-11-26 12:46:52 +0500

    connect rds

--- urbanthreads/urbanthreads/settings.py
+++ urbanthreads/urbanthreads/settings.py
@@ -12,8 +12,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import csv
+
+for row in (csv.reader(open(r'D:\ONGOING\clothing_brand_website\credentials.csv'))):
+    print(row)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -77,10 +82,14 @@ WSGI_APPLICATION = 'urbanthreads.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	'default':{
+		'ENGINE':'django.db.backends.mysql',
+		'NAME' : 'urbanthreadsDataBase',
+		'HOST':'database-ut.cw7kvs5capnk.us-east-1.rds.amazonaws.com',
+        'USER': 'admin',
+		'PASSWORD':row[0],
+		'PORT':'3306',
+	},
 }
 
 
